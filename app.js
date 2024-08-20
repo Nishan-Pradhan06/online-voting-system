a2a24d7e114ca32b01bcaaf59cd9bab82ef16098
nishanpradhan <nishanpradhan500@gmail.com>
2024-08-20 07:48:22 +0545

    folder structure

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs")
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render("index.ejs");
 })
 
 app.listen(port, function () {
-    console.log("Conneting.")
-    console.log("Conneting..")
-    console.log("Conneting...")
     console.log("Successfully connected port on🥳 " + port)
 })
